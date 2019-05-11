6f3c0df068ad097917d825812a5db019c1e62703
Dan Walkes <danwalkes@trellis-logic.com>
2019-05-11 14:56:20 -0600

    Initial commit
    Based on sources used in ECEN5823 Spring 2019 semester but with
    course specifics moved into configuration files and student names
    moved into .csv import files.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Overview
+This repository contains scripts which may be useful for courses which use
+[GitHub Classroom](https://classroom.github.com) for assignment submission.
+This is especially true for courses which use assignment series, where one assignment builds on the previous.
+It was originally developed for use in University of Colorado course
+[ECEN 5823](https://sites.google.com/colorado.edu/ecen5823/home).
+
+# Using These Scripts
+1. Add this repository as a git submodule on a repository used by instructors/student assistants for assignment grading.
+  This would typically be a private repository which contains completed assignments for reference by instructors and
+   teaching assistants.
+2. Add a scripts_config/classroom_roster.csv file in the parent repository containing the list of students exported from
+the Roster Mangement->Download section of your GitHub classroom.  You may need to prune this to remove teaching
+assistants and yourself from the list, as well as any students who haven't actually submitted an assignment
+(perhaps dropped the course).  Make sure you check this into a private repository to avoid sharing student information
+publicly.
+3. Setup your assignment and GithubClassroom using a scripts_config/assignconfig.txt file.  An example config file is
+provided in the assignconfig_example.txt file.
+4. Run ```pip install -r requirements.txt``` to install requirements in your python environment
+5. Run the scripts from the base directory using python github-classroom-scripts/scriptname.py so the script will work
+ on the parent repository and find the roster file and assignconfig.txt file, also so the git commands will work on the
+ appropriate repository.
+
+See sections below for individual script details.
+
+## prsetup.py
+
+This script works with an assignment series where assignments start with a base set of code and then build on each
+other throughout the semester.  It sets up pull request in each student's repository which allow you to easily compare
+the changes made between the previous/current assignments.  This pull request will not actually be merged, but will be
+a place where instructors and student assistants can add code review comments which students can see and respond to.
+
+The script creates:
+* A remote named  *student*_assignment*number*_remote pointing to the student's submission repository.
+* A local branch named *student*_assignment*number*_submission containing the content of the student's assignment
+submission.
+* Remote branches in the student's repository with the same branch names above, containing the current and previous
+submission or, in the case that this is the first assignment, the difference between the current submission and the
+assignment used as the base submission.
+
+The script also opens a page which compares the current and previous submission branches. Clicking the "Create PR"
+button on this page creates a pull request.
+
+Start by editing the variables at the top of the script to point to your github classroom page and assignments.
+
+Run the script with no arguments for usage instructions.
---
+++ prsetup.py
@@ -0,0 +1,169 @@
+# Python 3
+import subprocess
+import webbrowser
+import traceback
+import argparse
+import sys
+import csv
+import configparser
+assign = configparser.ConfigParser()
+
+ASSIGNCONFIG_FILE='scripts_config/assignconfig.txt'
+CLASSROSTER_FILE='scripts_config/classroom_roster.csv'
+import os
+exists = os.path.isfile(ASSIGNCONFIG_FILE)
+if exists:
+    assign.read(ASSIGNCONFIG_FILE)
+else:
+    print("Missing assignment configuration at " + ASSIGNCONFIG_FILE )
+    print("Please add this file with details about your GitHub Classroom Setup.")
+    print("See assignconfig_example.txt for an example.")
+    exit(1)
+
+error_students={}
+
+assign_name_git_url_prefix="git@github.com:"+ assign['DEFAULT']['GITHUB_CLASSROOM'] + "/"
+
+parser = argparse.ArgumentParser(
+                    description="Setup pull requests for students using github classroom for assignment submission.\n"
+                                "  By default with no arguments, just opens PR pages for each student submission.")
+parser.add_argument('--dry_run',dest='dry_run', action='store_true',
+                    help='Do a dry run only, printing the commands which would be executed')
+parser.add_argument('--log_cmds',dest='log_cmds', action='store_true',  default=False, help='Log all commands as run')
+parser.add_argument('--delete_local',dest='delete_local', action='store_true', default=False,
+                        help='Delete local repositories if they exist (to resync with master on remotes)')
+parser.add_argument('--create_local',dest='create_local', action='store_true', default=False,
+                        help='Create local repositories based on student submissions if they do not already exist')
+parser.add_argument('--push_remote',dest='push_remote', action='store_true', default=False,
+                        help='Push updated local repositories to remote submission repositories (for PR creation)')
+parser.add_argument('--no_open_pr_page',dest='open_pr_page', action='store_false', default=True,
+                        help='Skip opening pull request webpages for each student')
+parser.add_argument('--force_push',dest='force_push', action='store_true', default=False,
+                        help='force push to remote repository, to unconditionally update content there')
+
+args = parser.parse_args()
+
+def read_roster_students(csv_file):
+    '''
+    Read a CSV file with github_username column defined in the header (matching GitHub Classroom export format) and
+    return the corresponding list of students
+    :param csv_file: file exported from Github classrom, opened with read permissions, containing a column
+    "github_username"
+    :return: an array of students from the github_username column.
+    '''
+    csv_reader=csv.DictReader(csv_file)
+    students=[]
+    line_count=0
+    for row in csv_reader:
+        if line_count > 0:
+            students.append(row["github_username"])
+        line_count = line_count+1
+    return students
+
+def get_roster_students():
+    students=None
+    try:
+        with open(CLASSROSTER_FILE, mode='r') as csv_file:
+            students=read_roster_students(csv_file)
+    except FileNotFoundError:
+        print("Could not find classroom roster at " + CLASSROSTER_FILE)
+        print("Please run the script from a directory which contains this scripts_config folder and csv file."
+              "See README for details")
+        exit(1)
+    return students
+
+def cmd(cmdargs):
+    ret="bogus return" # just to prevent errors in dry run cases
+    if args.dry_run or args.log_cmds:
+        print(*cmdargs)
+        sys.stdout.flush()
+    if not args.dry_run:
+        ret=str(subprocess.check_output(cmdargs))
+    return ret
+
+def create_remote_if_not_existing(name,remote_url):
+    remotes=cmd(["git","remote"])
+    if remotes.find(name) == -1:
+        cmd(["git","remote","add",name,remote_url])
+
+def checkout_and_track_or_update(remote,local_branch,remote_branch):
+    branches=cmd(["git", "branch"])
+    cmd(["git", "fetch", remote])
+    if branches.find(local_branch) == -1:
+        cmd(["git", "fetch", remote, remote_branch+":"+local_branch])
+
+def delete_local_if_exists(local_branch):
+    branches=cmd(["git", "branch"])
+    if branches.find(local_branch):
+        cmd(["git", "branch", '-D', local_branch])
+
+def push_local_branch_to_remote_check_if_exists(remote,local_and_remote_branchname):
+    exists=False
+    remote_branches=cmd(["git", "branch", "-r"])
+    if remote_branches.find(remote+"/"+local_and_remote_branchname)!= -1 :
+        exists=True
+    if args.force_push:
+        cmd(["git", "push", "-f", remote, local_and_remote_branchname])
+    else:
+        cmd(["git", "push", remote, local_and_remote_branchname])
+    return exists
+
+
+def open_browser_at_url(url):
+    if not args.dry_run:
+        webbrowser.open(url)
+
+if args.dry_run:
+    print("Doing a dry run, not actually setting up any remotes or repos")
+
+students = get_roster_students()
+
+if len(students) == 0:
+    print("No students defined, check the format of student_list/classroom_roster.csv")
+    exit(1)
+
+for student in students:
+    try:
+        if assign.getboolean('DEFAULT','STARTS_WITH_PREV_ASSIGN'):
+            assign_prev_remote = student + "_assignment" + str(assign["DEFAULT"]["NUMBER_PREV"]) + "_remote"
+            assign_prev_branch = student + "_assignment" + str(assign["DEFAULT"]["NUMBER_PREV"]) + "_submission"
+            assign_base_repo_full = assign_name_git_url_prefix+assign["DEFAULT"]["NAME_PREV"]+"-"+student+".git"
+        else:
+            assign_prev_remote = assign["DEFAULT"]["BASE_REPO"]
+            assign_prev_branch = assign["DEFAULT"]["BASE_REPO_BRANCH"]
+            assign_base_repo_full = assign_name_git_url_prefix+assign["DEFAULT"]["BASE_REPO"] +".git"
+
+        assign_current_remote = student + "_assignment" + str(assign["DEFAULT"]["NUMBER_CURRENT"]) + "_remote"
+        assign_current_branch = student + "_assignment" + str(assign["DEFAULT"]["NUMBER_CURRENT"]) + "_submission"
+        if args.delete_local:
+            delete_local_if_exists(assign_current_branch)
+        if args.create_local:
+            create_remote_if_not_existing(assign_prev_remote,
+                                          assign_base_repo_full)
+            create_remote_if_not_existing(assign_current_remote,
+                                          assign_name_git_url_prefix + assign["DEFAULT"]["NAME_CURRENT"]
+                                                + "-" + student + ".git")
+            cmd(["git","fetch",assign_prev_remote])
+            cmd(["git", "fetch",assign_current_remote])
+            checkout_and_track_or_update(assign_prev_remote,assign_prev_branch,"master")
+            checkout_and_track_or_update(assign_current_remote,assign_current_branch,"master")
+        if args.push_remote:
+            push_local_branch_to_remote_check_if_exists(assign_current_remote,assign_prev_branch)
+            push_local_branch_to_remote_check_if_exists(assign_current_remote,assign_current_branch)
+        if args.open_pr_page:
+            open_browser_at_url("https://github.com/" + assign['DEFAULT']['GITHUB_CLASSROOM'] +"/" +
+                                assign['DEFAULT']['NAME_CURRENT'] + "-" +
+                                student + "/compare/"+assign_prev_branch+"..."+assign_current_branch+"?expand=1")
+    except Exception as err:
+        print("Could not complete assignment PR setup for student {}".format(student))
+        error_students[student]=str(err)+traceback.format_exc()
+
+if len(error_students.keys()) != 0:
+    print("The following {} students had errors attempting to create pull requests:".format(len(error_students.keys())))
+    for student in error_students.keys():
+        print("Student {}: error {}".format(student,error_students[student]))
+else:
+    print("All student actions complete with success!")
+
+
+
